38fcbd740bd8d30f552a9573b97728b7bae16703
Melissa O'Connor <mmoconnor23@gmail.com>
2015-06-21 18:07:58 -0400

    trying to fix bower with heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require( 'express' );
 var server = express();
 
-server.use( express.static( __dirname + '/bower_components' ) );
 server.use( express.static( __dirname + '/app' ) );
+
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 var port = process.env.PORT;
 //var port = 5000;
