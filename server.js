9168c0510f09e011e58f83468e419f6f9046f8ee
Melissa O'Connor <mmoconnor23@gmail.com>
2015-06-22 22:18:04 -0400

    fixing tab height for tech section

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = express();
 server.use( express.static( __dirname + '/app' ) );
 
 var port = process.env.PORT;
-//var port = 5000;
+// var port = 5000;
 
 server.listen( port, function() {
   console.log( 'server listening on port ' + port );
