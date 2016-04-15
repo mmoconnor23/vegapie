28b126da44ae470535634db4ff5500c784186d6f
Melissa O'Connor <mmoconnor23@gmail.com>
2016-04-14 22:35:17 -0400

    delete, change files a little

--- app/app.controller.js
+++ app/app.controller.js
@@ -2,60 +2,9 @@
 
 function appCtrl( appState ) {
 	var vm = this;
-	
-	//vm.myState = appState.getAppState();
-	//vm.selectedTabIndex = vm.myState.selectedTabIndex;
-
-	vm.tabs = [
-		{
-			name: 'Home',
-			link: 'home'
-		},
-		{
-			name: 'About',
-			link: 'about'
-		},
-		{
-			name: 'Tech',
-			link: 'tech'
-		}
-	];
-
-	vm.buttons = [
-		{
-			label: 'Github',
-			href: 'http://github.com/mmoconnor23',
-			icon: 'img/webicons-master/webicons/webicon-github.svg'
-		},
-		{
-			label: 'Twitter',
-			href: 'http://twitter.com/bissamissa',
-			icon: 'img/webicons-master/webicons/webicon-twitter.svg'
-		},
-		{
-			label: 'LinkedIn',
-			href: 'http://www.linkedin.com/pub/melissa-o-connor/35/201/80/en',
-			icon: 'img/webicons-master/webicons/webicon-linkedin.svg'
-		},
-		{
-			label: 'E-mail me',
-			href: 'mailto:mmoconnor23@gmail.com',
-			icon: 'img/webicons-master/webicons/webicon-mail.svg'
-		}
-	];
-
-	// vm.onTabSelected = function( tab ) {
-	// 	//console.log( tab );
-	// 	console.log( 'selected: ' + vm.selectedTabIndex );
-	// };
-
-	// vm.onTabDeselected = function( tab ) {
-	// 	//console.log( tab );
-	// 	console.log( 'deselected: ' + vm.selectedTabIndex );
-	// };
 }
 
-appCtrl.$inject = [ 'appState' ];
+appCtrl.$inject = [];
 
-angular.module('mySite')
+angular.module('vegapie')
 .controller( 'appCtrl', appCtrl );
--- app/app.js
+++ app/app.js
@@ -1,21 +1,17 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('mySite', [
-  'mySite.home',
-  'mySite.tech',
-  'mySite.about',
-  'ngMaterial',
+angular.module('vegapie', [
   'ui.router'
-])
-.run( [ '$state', function( $state ) {
-	$state.transitionTo( 'home' );
-}])
-.config( [ '$urlRouterProvider', function( $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( 'home' );
-}])
-.config( [ '$mdThemingProvider', function( $mdThemingProvider ) {
-	$mdThemingProvider.theme( 'default' )
-		.primaryPalette( 'deep-purple' )
-		.accentPalette( 'teal' );
-}]);
+]);
+// .run( [ '$state', function( $state ) {
+// 	$state.transitionTo( 'home' );
+// }])
+// .config( [ '$urlRouterProvider', function( $urlRouterProvider ) {
+//   $urlRouterProvider.otherwise( 'home' );
+// }])
+// .config( [ '$mdThemingProvider', function( $mdThemingProvider ) {
+// 	$mdThemingProvider.theme( 'default' )
+// 		.primaryPalette( 'deep-purple' )
+// 		.accentPalette( 'teal' );
+// }]);
